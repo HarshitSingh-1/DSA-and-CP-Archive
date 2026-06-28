bf02019a703bd7ddc26e1391ff290b9df09c7f04
Harshit Singh <harshit.singh.swe@gmail.com>
2026-06-28 12:24:49 +0530

    Solved questions on codeforces

---
+++ 466A_Cheap-Travel.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m,a,b;
+    cin>>n>>m>>a>>b;
+    if(b<a*m){
+        cout<<(n/m)*b+min((n%m)*a,b);
+    } else {
+        cout<<n*a;
+    }
+    return 0;
+}
